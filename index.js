a54afa54e2a5dfa66db72f41a83bc7c7b3c21e45
KishoreDevolper <kishore280420@gmail.com>
2023-05-16 11:03:21 +0530

    code update

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const process = require('process');
-const dotenv = require('dotenv');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const process = require("process");
+const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const routerService = require('./app/router/router');
-const {errHandle} = require("./app/middlewares/errorHandler");
+const routerService = require("./app/router/router");
+const { errHandle } = require("./app/middlewares/errorHandler");
 const swaggerUi = require("swagger-ui-express");
 const app = express();
-const {instantiateAWSS3} = require("./app/externalServices/awsService.js")
+const { instantiateAWSS3 } = require("./app/externalServices/awsService.js");
 
 //load environment variable
-dotenv.config({path:path.join(process.cwd(), `${process.argv[2]}`)});
+dotenv.config({ path: path.join(process.cwd(), `${process.argv[2]}`) });
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Headers","*");
-    next();
-})
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 //app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
-app.use(express.static(__dirname+"/assets"));
+app.use(express.static(__dirname + "/assets"));
 
 var numOfRequest = 1;
 
-app.use((req,res,next)=>{
-    req.startTime = Date.now();
-    req.numOfRequest = numOfRequest;
-    numOfRequest++;
-    console.log("Hit :" + req.originalUrl);
-    next();
-})
+app.use((req, res, next) => {
+  req.startTime = Date.now();
+  req.numOfRequest = numOfRequest;
+  numOfRequest++;
+  console.log("Hit :" + req.originalUrl);
+  next();
+});
 
-//DB connection 
+//DB connection
 const connectToMongo = async () => {
   await mongoose.connect(process.env.MONGO_URL);
   console.log("Connected to MongoDB Sucessfully!!");
@@ -50,17 +50,16 @@ instantiateAWSS3();
 
 const port = process.env.PORT;
 
-app.use("/api",routerService);
+app.use("/api", routerService);
 
 app.listen(port, () => {
-    console.log(
-      `Microservice ${process.env.SERVICE_NAME} is running on port ${port}.`
-    );
-  });
-  
-  app.get("/", (req, res) => {
-    res.send("successfully connnected");
-  });
-  
-  app.use(errHandle);
-  
+  console.log(
+    `Microservice ${process.env.SERVICE_NAME} is running on port ${port}.`
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("successfully connnected");
+});
+
+app.use(errHandle);
