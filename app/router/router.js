0de95272ce0ffe02d6ba948101bb5c1d68dfb47a
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-06-05 19:12:04 +0530

    Update router.js

--- app/router/router.js
+++ app/router/router.js
@@ -5,7 +5,7 @@ var upload = multer();
 const {
   verifyAdminToken,
   verifyauthorizedPersonToken,
-  verifyAdminRole
+  verifyAdminRole,
 } = require("../middlewares/authentication");
 const {
   adminloginSchema,
@@ -25,11 +25,11 @@ const {
   resetPasswordSchema,
   verifyOTPSchema,
   authorizedPersonloginbyIdSchema,
-  addClientFamilySchema ,
+  addClientFamilySchema,
   clientFamilyProfileSchema,
-  clientFamilyListSchema,  
+  clientFamilyListSchema,
   updateClientFamilyProfileSchema,
-  forgotPasswordLoginIdSchema
+  forgotPasswordLoginIdSchema,
 } = require("../validator/validatator");
 const {
   adminLogin,
@@ -55,54 +55,175 @@ const {
   authorizedPersonLoginById,
   authorizedPersonMailLoginById,
 } = require("../controllers/authorizedPerson.controller");
-
-const  {addClientFamilyPerson, getClientPersonProfile,clientFamilyList,updateClientFamilyProfile,deleteClientFamily} = require("../controllers/clientFamily.controller")
+const {
+  addClientFamilyPerson,
+  getClientPersonProfile,
+  clientFamilyList,
+  updateClientFamilyProfile,
+  deleteClientFamily,
+} = require("../controllers/clientFamily.controller");
 const { errHandle } = require("../helpers/index");
 const { routes } = require("../routes/routes");
 const router = Router();
-
 //admin related api's
 router.post(routes.v1.admin.login, [adminloginSchema], errHandle(adminLogin));
 //router.post(routes.v1.admin.logout, [verifyAdminToken], errHandle(adminLogout));
 router.post(routes.v1.admin.sendOTP, [sendOTPSchema], errHandle(sendOTP));
 router.post(routes.v1.admin.verifyOTP, [verifyOTPSchema], errHandle(verifyOTP));
-router.post( routes.v1.admin.forgotPassword, [resetPasswordSchema], errHandle(forgot_password));
-router.post( routes.v1.admin.resetPassword, [verifyAdminToken], errHandle(resetPassword));
+router.post(
+  routes.v1.admin.forgotPassword,
+  [resetPasswordSchema],
+  errHandle(forgot_password)
+);
+router.post(
+  routes.v1.admin.resetPassword,
+  [verifyAdminToken],
+  errHandle(resetPassword)
+);
 router.post(routes.v1.admin.addProfile, [addAdminSchema], errHandle(addAdmin));
-router.get( routes.v1.admin.getProfile,[verifyAdminToken,verifyAdminRole("staffManagement", "VIEW"), getAdminProfileSchema],errHandle(getAdminProfile));
+router.get(
+  routes.v1.admin.getProfile,
+  [
+    verifyAdminToken,
+    verifyAdminRole("staffManagement", "VIEW"),
+    getAdminProfileSchema,
+  ],
+  errHandle(getAdminProfile)
+);
 router.get(routes.v1.admin.getAdmin, errHandle(getProfile));
-router.put(routes.v1.admin.updateProfile,[verifyAdminToken,verifyAdminRole("staffManagement", "EDIT"), updateAdminProfileSchema],errHandle(updateAdminProfile));
-router.delete(routes.v1.admin.delete, [verifyAdminToken,verifyAdminRole("staffManagement", "DELETE"), getAdminProfileSchema],errHandle(deleteAdmin));
-router.get(routes.v1.admin.list,[verifyAdminToken, adminListSchema],errHandle(adminList));
-
+router.put(
+  routes.v1.admin.updateProfile,
+  [
+    verifyAdminToken,
+    verifyAdminRole("staffManagement", "EDIT"),
+    updateAdminProfileSchema,
+  ],
+  errHandle(updateAdminProfile)
+);
+router.delete(
+  routes.v1.admin.delete,
+  [
+    verifyAdminToken,
+    verifyAdminRole("staffManagement", "DELETE"),
+    getAdminProfileSchema,
+  ],
+  errHandle(deleteAdmin)
+);
+router.get(
+  routes.v1.admin.list,
+  [verifyAdminToken, adminListSchema],
+  errHandle(adminList)
+);
 // authorizedPerson related api's
-router.post(routes.v1.authorizedPerson.login,[authorizedPersonloginSchema],errHandle(authorizedPersonLogin)); //authorizedPerson login using mobileNumber
-router.post(routes.v1.authorizedPerson.verifyOTP, [authorizedPersonVerifyOTPSchema], errHandle(authorizedPersonverifyOTP)); //authorized person verify Otp through mobileNumber
-router.post( routes.v1.authorizedPerson.sendOTP,[sendOTPSchema], errHandle(authorizedPersonLogin)); //authorized person send Otp through authorizedPersonID
-router.post(routes.v1.authorizedPerson.forgotPasswordverifyOTP,[forgotPasswordVerifyOTPSchema],errHandle(authorizedPersonverifyOTP)); //authorized person verify Otp through authorizedPersonID
-router.post(routes.v1.authorizedPerson.forgotPasswordLoginIdMail,[forgotPasswordLoginIdSchema],errHandle(authorizedPersonMailLoginById)); //authorized person verify Otp through authorizedPersonID
-router.post(routes.v1.authorizedPerson.loginById,[authorizedPersonloginbyIdSchema],errHandle(authorizedPersonLoginById)); //authorized person login via id and password
-router.post(routes.v1.authorizedPerson.addProfile, [addAuthorizedPersonSchema], errHandle(addauthorizedPerson));
-router.get( routes.v1.authorizedPerson.getProfile,[verifyauthorizedPersonToken, getAuthorizedPersonProfileSchema], errHandle(getauthorizedPersonProfile));
-router.get( routes.v1.authorizedPerson.getProfileById,errHandle(getauthorizedPersonProfile));
-router.delete( routes.v1.authorizedPerson.delete,[verifyauthorizedPersonToken, updateAuthorizedPersonProfileSchema], errHandle(deleteauthorizedPerson));
-router.put(routes.v1.authorizedPerson.updateProfile,[verifyauthorizedPersonToken, updateAuthorizedPersonProfileSchema],errHandle(updateauthorizedPersonProfile));
-router.get(routes.v1.authorizedPerson.list,[verifyAdminToken, authorizedPersonListSchema],errHandle(authorizedPersonList));
-
-
+router.post(
+  routes.v1.authorizedPerson.login,
+  [authorizedPersonloginSchema],
+  errHandle(authorizedPersonLogin)
+); //authorizedPerson login using mobileNumber
+router.post(
+  routes.v1.authorizedPerson.verifyOTP,
+  [authorizedPersonVerifyOTPSchema],
+  errHandle(authorizedPersonverifyOTP)
+); //authorized person verify Otp through mobileNumber
+router.post(
+  routes.v1.authorizedPerson.sendOTP,
+  [sendOTPSchema],
+  errHandle(authorizedPersonLogin)
+); //authorized person send Otp through authorizedPersonID
+router.post(
+  routes.v1.authorizedPerson.forgotPasswordverifyOTP,
+  [forgotPasswordVerifyOTPSchema],
+  errHandle(authorizedPersonverifyOTP)
+); //authorized person verify Otp through authorizedPersonID
+router.post(
+  routes.v1.authorizedPerson.forgotPasswordLoginIdMail,
+  [forgotPasswordLoginIdSchema],
+  errHandle(authorizedPersonMailLoginById)
+); //authorized person verify Otp through authorizedPersonID
+router.post(
+  routes.v1.authorizedPerson.loginById,
+  [authorizedPersonloginbyIdSchema],
+  errHandle(authorizedPersonLoginById)
+); //authorized person login via id and password
+router.post(
+  routes.v1.authorizedPerson.addProfile,
+  [addAuthorizedPersonSchema],
+  errHandle(addauthorizedPerson)
+);
+router.get(
+  routes.v1.authorizedPerson.getProfile,
+  [verifyauthorizedPersonToken, getAuthorizedPersonProfileSchema],
+  errHandle(getauthorizedPersonProfile)
+);
+router.get(
+  routes.v1.authorizedPerson.getProfileById,
+  errHandle(getauthorizedPersonProfile)
+);
+router.delete(
+  routes.v1.authorizedPerson.delete,
+  [verifyauthorizedPersonToken, updateAuthorizedPersonProfileSchema],
+  errHandle(deleteauthorizedPerson)
+);
+router.put(
+  routes.v1.authorizedPerson.updateProfile,
+  [verifyauthorizedPersonToken, updateAuthorizedPersonProfileSchema],
+  errHandle(updateauthorizedPersonProfile)
+);
+router.get(
+  routes.v1.authorizedPerson.list,
+  [verifyAdminToken, authorizedPersonListSchema],
+  errHandle(authorizedPersonList)
+);
 //client family related api
-router.post(routes.v1.clientFamily.addProfile,[verifyauthorizedPersonToken,addClientFamilySchema],addClientFamilyPerson)
-router.put(routes.v1.clientFamily.updateProfile,[updateClientFamilyProfileSchema],updateClientFamilyProfile)
- router.get(routes.v1.clientFamily.getProfile,[clientFamilyProfileSchema],getClientPersonProfile)
- router.get(routes.v1.clientFamily.list,[clientFamilyListSchema],clientFamilyList)
-  router.delete(routes.v1.clientFamily.delete,[clientFamilyProfileSchema],deleteClientFamily)
-
+router.post(
+  routes.v1.clientFamily.addProfile,
+  [verifyauthorizedPersonToken, addClientFamilySchema],
+  addClientFamilyPerson
+);
+router.put(
+  routes.v1.clientFamily.updateProfile,
+  [updateClientFamilyProfileSchema],
+  updateClientFamilyProfile
+);
+router.get(
+  routes.v1.clientFamily.getProfile,
+  [clientFamilyProfileSchema],
+  getClientPersonProfile
+);
+router.get(
+  routes.v1.clientFamily.list,
+  [clientFamilyListSchema],
+  clientFamilyList
+);
+router.delete(
+  routes.v1.clientFamily.delete,
+  [clientFamilyProfileSchema],
+  deleteClientFamily
+);
 //client admin family related api
-router.post(routes.v1.clientAdminFamily.addProfile,[verifyauthorizedPersonToken,addClientFamilySchema],addClientFamilyPerson)
-router.put(routes.v1.clientAdminFamily.updateProfile,[updateClientFamilyProfileSchema],updateClientFamilyProfile)
- router.get(routes.v1.clientAdminFamily.getProfile,[clientFamilyProfileSchema],getClientPersonProfile)
- router.get(routes.v1.clientAdminFamily.list,[clientFamilyListSchema],clientFamilyList)
-  router.delete(routes.v1.clientAdminFamily.delete,[clientFamilyProfileSchema],deleteClientFamily)
-
-
-  module.exports = router;
+router.post(
+  routes.v1.clientAdminFamily.addProfile,
+  [verifyauthorizedPersonToken, addClientFamilySchema],
+  addClientFamilyPerson
+);
+router.put(
+  routes.v1.clientAdminFamily.updateProfile,
+  [updateClientFamilyProfileSchema],
+  updateClientFamilyProfile
+);
+router.get(
+  routes.v1.clientAdminFamily.getProfile,
+  [clientFamilyProfileSchema],
+  getClientPersonProfile
+);
+router.get(
+  routes.v1.clientAdminFamily.list,
+  [clientFamilyListSchema],
+  clientFamilyList
+);
+router.delete(
+  routes.v1.clientAdminFamily.delete,
+  [clientFamilyProfileSchema],
+  deleteClientFamily
+);
+module.exports = router;
