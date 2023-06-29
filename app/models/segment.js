5cc11e879a032d160cc27777a3ae308b2775fd7b
kishoreDevolper <kishore.t@doodleblue.in>
2023-06-29 14:29:34 +0530

    code update

--- app/models/segment.js
+++ app/models/segment.js
@@ -31,7 +31,14 @@ const segmentSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
-
+segmentSchema.pre('save', async function (next) {
+    var doc = this;
+    let counter = await Sequence.findOneAndUpdate({ type: 'segment' }, { $inc: { count: 1 } })
+    doc.segmentId = (counter.count + 1).toString().padStart(6, '0').toString();;
+    next();
+  
+  });
+  
 const segment = mongoose.model("segment", segmentSchema);
 // export default Admin;
 module.exports = { segment };
